9c695673646489fb799c7cc455ce9e8bd9a367e0
mboss <mboss@ethz.ch>
2019-12-10 15:15:57 +0100

    Reset exercises

--- src/exercises/Abstand.java
+++ src/exercises/Abstand.java
@@ -1,10 +1,12 @@
 package exercises;
 
+import java.util.Arrays;
+
 /**
  * Created by mikeb on 12/3/2019
  */
 class Abstand {
 	static int abstand(int[] ms, int[] ns) {
-		return 0;
+		return 42;
 	}
 }
